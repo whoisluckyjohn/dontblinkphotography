8eeeb1ec52703b7802d82dfd1ff0591c4dbc3604
MurageKabui <dennisk@zainahtech.com>
2024-04-26 10:04:33 +0000

    Update README.md

--- docs/assets/vendor/php-email-form/validate.js
+++ docs/assets/vendor/php-email-form/validate.js
@@ -2,16 +2,25 @@
     "use strict";
 
     let sendMessageButton = document.querySelector('.php-email-form button[type="submit"]');
+    let sentMessage = document.querySelector('.sent-message');
+
     sendMessageButton.addEventListener('click', function(event) {
         event.preventDefault();
 
         let name = document.querySelector('.php-email-form input[name="name"]').value;
         let message = document.querySelector('.php-email-form textarea[name="message"]').value;
 
-        let encodedMessage = encodeURIComponent(`My name is ${name}. ${message}`);
-        let whatsappLink = `https://wa.me/254757169030?text=${encodedMessage}`;
+        // Display "Message Sent" status
+        sentMessage.classList.add('d-block');
 
-        window.open(whatsappLink, '_blank');
+        setTimeout(function() {
+            // Hide "Message Sent" status after one second
+            sentMessage.classList.remove('d-block');
+            
+            let encodedMessage = encodeURIComponent(`My name is ${name}. ${message}`);
+            let whatsappLink = `https://wa.me/254757169030?text=${encodedMessage}`;
+            window.open(whatsappLink, '_blank');
+        }, 1000); // Display "Message Sent" status for 1 second (1000 milliseconds)
     });
 
 })();
